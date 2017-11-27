5154c9a8e73e06e58207321b9be83b3c36d188a4
Christian Wiedemann <c.wiedemann@profi-ag.de>
2017-11-27 21:04:00 +0100

    ametest

--- testgame.py
+++ testgame.py
@@ -4,11 +4,8 @@ from pygame.locals import *
 pygame.init()
 
 while(True):
-   for event in pygame.event.get():
-      if (event.type == KEYDOWN):
-         print event
-         print event.key
-         if (event.key == K_KP0):
-			print "numpad 0"
-         if (event.key == K_KP1):
-			print "numpad 1"
+    for event in pygame.event.get():
+        print event.type
+        if (event.type == KEYDOWN):
+            print event
+            print event.key
