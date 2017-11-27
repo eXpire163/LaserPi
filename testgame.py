5bccc534f179b8e0e0227b0048a0487c86293b81
Christian Wiedemann <c.wiedemann@profi-ag.de>
2017-11-27 21:20:33 +0100

    update

--- testgame.py
+++ testgame.py
@@ -3,7 +3,12 @@ import curses
 
 # Here starts the code to make the robot move
 stdscr = curses.initscr()
+curses.cbreak()
+stdscr.keypad(1)
+
+stdscr.nodelay(1)  #nodelay(1) give us a -1 back when nothing is pressed
+
 
 while True:
     stdscr.refresh()
-    print stdscr.getch()
+        print stdscr.getch()
