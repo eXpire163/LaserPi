0c2c10989b91e5a5c2d454bfbf6856f44d00ac0f
Christian Wiedemann <c.wiedemann@profi-ag.de>
2017-11-27 21:29:59 +0100

    clone test

--- testgame.py
+++ testgame.py
@@ -1,15 +1,46 @@
 import curses
 
-
 # Here starts the code to make the robot move
 stdscr = curses.initscr()
+curses.cbreak()
+stdscr.keypad(1)
 
-
+stdscr.addstr(0,10,"Hit 'q' to quit")
 stdscr.nodelay(1)  #nodelay(1) give us a -1 back when nothing is pressed
+Richtung = ' '
+while Richtung != ord('q'):
+	#Start of while loop
+	stdscr.refresh()
 
-
-while True:
-    stdscr.refresh()
-    Richtung = stdscr.getch() #Gets the key which is pressed
+	Richtung = stdscr.getch() #Gets the key which is pressed
+		
 	stdscr.addch(20,25,Richtung)
-    print Richtung
+	
+	if Richtung == ord('e'):
+		print "StopMotors()"
+
+	if  Richtung == ord('a'):
+		print "Left()"
+
+	if Richtung == ord('s'):
+		print "Backwards()"
+
+	if Richtung == ord('d'):
+		print "Right()"
+	
+	if Richtung == ord('w'):
+		print "Forwards()"
+	
+	if Richtung == int('-1'):
+		print "StopMotors()"
+		
+	time.sleep(0.04) #I need the timesleep because if not the robot will get a -1 all the time and not move to fast while loop I think, have to work on this.
+	#End of while loop
+	
+#Important to set everthing back by end of the script
+
+curses.nocbreak()
+stdscr.keypad(0)
+curses.endwin()
+StopMotors()
+GPIO.cleanup()
