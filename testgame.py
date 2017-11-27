b631251e1ee50dffc7662e5c53cd2bc203ced584
Christian Wiedemann <c.wiedemann@profi-ag.de>
2017-11-27 21:25:15 +0100

    add richtung

--- testgame.py
+++ testgame.py
@@ -3,12 +3,13 @@ import curses
 
 # Here starts the code to make the robot move
 stdscr = curses.initscr()
-curses.cbreak()
-stdscr.keypad(1)
+
 
 stdscr.nodelay(1)  #nodelay(1) give us a -1 back when nothing is pressed
 
 
 while True:
     stdscr.refresh()
-        print stdscr.getch()
+    Richtung = stdscr.getch() #Gets the key which is pressed
+	stdscr.addch(20,25,Richtung)
+    print Richtung
